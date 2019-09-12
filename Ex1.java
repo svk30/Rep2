4d1fab272342fb6d4f5e84065da91e0f287fa5d6
svk30 <51742625+svk30@users.noreply.github.com>
2019-09-12 20:47:52 +0530

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,7 +1,7 @@
 public class Ex1 {
 
  public static void main(String args[]){
-      System.out.println("First Example");
+      System.out.println("------------First Example--------------");
  }
 
 }
